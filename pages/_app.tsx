0469b7bc7bc3de5ab84f9261b5487c5d4c0b380a
Ethan James <ethan.e.james@gmail.com>
2022-07-31 22:26:05 +0100

    prettier formatting

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,15 @@
-import '../styles/globals.css'
-import { useEffect, useState } from 'react';
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import { useEffect, useState } from "react";
+import type { AppProps } from "next/app";
 import { UserProvider } from "@auth0/nextjs-auth0";
 import { loadStripe } from "@stripe/stripe-js";
 import { Elements } from "@stripe/react-stripe-js";
 import { Appearance } from "@stripe/stripe-js/types";
 
-const appearance: Appearance = { theme: 'night' };
-const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY || "");
-
+const appearance: Appearance = { theme: "night" };
+const stripePromise = loadStripe(
+  process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY || ""
+);
 
 function MyApp({ Component, pageProps }: AppProps) {
   const [clientSecret, setClientSecret] = useState("");
@@ -26,8 +27,8 @@ function MyApp({ Component, pageProps }: AppProps) {
 
   const options = { clientSecret, appearance };
 
-  if (!clientSecret) return "Loading..."
-  
+  if (!clientSecret) return "Loading...";
+
   return (
     <UserProvider>
       <Elements options={options} stripe={stripePromise}>
@@ -37,4 +38,4 @@ function MyApp({ Component, pageProps }: AppProps) {
   );
 }
 
-export default MyApp
+export default MyApp;
